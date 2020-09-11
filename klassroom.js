2efc2742629f1af43fefe81a99d4a5bf62d375de
Toldwin <toldwin@gmail.com>
2020-09-11 18:53:04 +0200

    Creation

---
+++ klassroom.js
@@ -0,0 +1,21 @@
+setInterval(myMethod, 2000);
+
+function myMethod( )
+{
+  data = document.getElementsByClassName("fancybox-content");
+  
+  var i;
+  for (i = 0; i < data.length; i++) {
+	if(data[i].children.length==1){
+		
+		var aLink = document.createElement("a");   // Create a <button> element
+		var link = document.createTextNode("Lien de téléchargement"); 
+		// Append the text node to anchor element. 
+		aLink.appendChild(link); 
+		aLink.title = "(click droit -> enregistrer sous)";
+		aLink.href = data[i].firstElementChild.src;  
+		aLink.style="position:relative;z-index:1000;"
+		data[i].appendChild(aLink); 
+	}
+  }
+}
