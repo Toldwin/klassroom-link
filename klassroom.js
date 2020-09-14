f5bc1beaa8be60488cc531ec87755213ba9b8127
Toldwin <toldwin@gmail.com>
2020-09-14 17:51:41 +0200

    Update klassroom.js

--- klassroom.js
+++ klassroom.js
@@ -12,7 +12,7 @@ function myMethod( )
 		var link = document.createTextNode("Lien de téléchargement"); 
 		// Append the text node to anchor element. 
 		aLink.appendChild(link); 
-		aLink.title = "(click droit -> enregistrer sous)";
+		aLink.title = "(click droit -> Enregistrer le lien sous...)";
 		aLink.href = data[i].firstElementChild.src;  
 		aLink.style="position:relative;z-index:1000;"
 		data[i].appendChild(aLink); 
