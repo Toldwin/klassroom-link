dd2cc74113a22b0377d056c86cba9b7bad6ac65e
Toldwin <toldwin@gmail.com>
2020-10-31 14:24:14 +0100

    Lorsque je vois sur un site : <div class="ril-download-blocker ril__downloadBlocker"></div> je deviens tout rouge

--- klassroom.js
+++ klassroom.js
@@ -1,19 +1,18 @@
 setInterval(myMethod, 2000);
-
 function myMethod( )
 {
-  data = document.getElementsByClassName("fancybox-content");
+  data = document.getElementsByClassName("ril-image-current ril__image ril__imageDiscourager");
   
   var i;
   for (i = 0; i < data.length; i++) {
 	if(data[i].children.length==1){
-		
+				
 		var aLink = document.createElement("a");   // Create a <button> element
 		var link = document.createTextNode("Lien de téléchargement"); 
 		// Append the text node to anchor element. 
 		aLink.appendChild(link); 
 		aLink.title = "(click droit -> enregistrer sous)";
-		aLink.href = data[i].firstElementChild.src;  
+		aLink.href = data[i].style.backgroundImage.replaceAll('url(\"','').replaceAll('\")','');
 		aLink.style="position:relative;z-index:1000;"
 		data[i].appendChild(aLink); 
 	}
