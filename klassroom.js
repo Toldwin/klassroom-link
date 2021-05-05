21b8dd31efa422275626e4a44c9d942608f56b58
Toldwin <toldwin@gmail.com>
2021-05-05 18:18:12 +0200

    Update klassroom.js
    Mis à jour et fonctionnel au 05/05/2021

--- klassroom.js
+++ klassroom.js
@@ -1,27 +1,30 @@
-setInterval(myMethod, 2000);
+setInterval(myMethod, 500);
 
 function myMethod( )
 {
-  data = document.getElementsByClassName("ril-image-current ril__image ril__imageDiscourager");
+  //data = document.getElementsByClassName("ril-image-current ril__image ril__imageDiscourager");
+  data = document.getElementsByClassName("ril-image-current ril__image");
   
   var i;
   for (i = 0; i < data.length; i++) {
-	if(data[i].children.length==1){
+	if(data[i].children.length==0){
 	
-		data[i].appendChild(document.createElement('br'));
-		data[i].appendChild(document.createElement('br'));
-		data[i].appendChild(document.createElement('br'));
-		data[i].appendChild(document.createElement('br'));
-		data[i].appendChild(document.createElement('br'));
-		
-		var aLink = document.createElement("a");   // Create a <button> element
+		var aLink = document.createElement("a");   // Create a link element
 		var link = document.createTextNode("Lien de téléchargement"); 
 		// Append the text node to anchor element. 
 		aLink.appendChild(link); 
-		aLink.href = data[i].style.backgroundImage.replaceAll('url(\"','').replaceAll('\")','');
+		aLink.href = data[i].src;
 		aLink.title = "(click droit -> Enregistrer le lien sous...)";
 		aLink.style="position:relative;z-index:1000;"
-		data[i].appendChild(aLink); 
+		aLink.id="addedLink";
+		
+		// Remove existing link
+		var existingLink=document.getElementById("addedLink");
+		if(existingLink){
+			existingLink.parentElement.removeChild(existingLink);
+		}
+		// Add the updated one
+		data[i].parentElement.appendChild(aLink); 
 	}
   }
 }
